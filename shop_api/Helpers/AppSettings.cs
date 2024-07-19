c6202bcbcf63a400733490234c4f216ef0b564af
salmanek123 <salmanckd82@gmail.com>
2024-07-19 14:03:54 +0530

    Add appsettings.Development.json to .gitignore

--- shop_api/Helpers/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace shop_api.Helper
-{
-    public class AppSettings
-    {
-        public bool DebugMode { get; set; } = false;
-        public string DbConnection { get; set; }
-
-    }
-}
